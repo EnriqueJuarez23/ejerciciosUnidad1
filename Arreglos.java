4632e4bd9c42bb01244317ca98089cf527cfbfe8
royxhry <rororoy1257@gmail.com>
2024-09-15 21:34:52 -0600

    royArreglos

---
+++ Arreglos.java
@@ -0,0 +1,14 @@
+class Arreglos{
+    public static void main(String[]args){
+        int[]nums={0,1,2,3,4,5,6,7,8,9};
+        String []animales={"perro","gato","leon"};
+        
+        for(int i=0;i<nums.length;i++){
+            System.out.println(nums[i]);
+        }
+        for(int b=0;b<animales.length;b++){
+            System.out.println(animales[b]);
+        }
+    }
+}
+        
